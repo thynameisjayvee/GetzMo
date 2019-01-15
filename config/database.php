65d9cb0a64c47b2072bb2c08a5d6d6ea6d00a311
Jayvee Tolibas <jayveetolibas@gmail.com>
2019-01-15 16:26:12 +0800

    database confug

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_3c53440aa2bca39'),
+            'username' => env('DB_USERNAME', 'b50cce04035aeb'),
+            'password' => env('DB_PASSWORD', '10bb1eed'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
